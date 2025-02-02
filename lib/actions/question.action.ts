98f214ec9e182e0ff29a06a07d289ee43672c052
Lenur Azizov <lenur29azizov@gmail.com>
2025-02-02 19:04:03 +0200

    feat: Implemented Question Details Page

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -12,8 +12,11 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
+import ROUTES from "@/constants/routes";
+import { revalidatePath } from "next/cache";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -203,7 +206,9 @@ export async function getQuestion(
   const { questionId } = validationResult.params!;
 
   try {
-    const question = await Question.findById(questionId).populate("tags");
+    const question = await Question.findById(questionId)
+      .populate("tags")
+      .populate("author", "_id name image");
 
     if (!question) {
       throw new Error("Question not found");
@@ -278,6 +283,42 @@ export async function getQuestions(
     return {
       success: true,
       data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(
+  params: IncrementViewsParams
+): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      throw new Error("Question not found");
+    }
+
+    question.views += 1;
+
+    await question.save();
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: { views: question.views },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
--- lib/utils.ts
+++ lib/utils.ts
@@ -7,39 +7,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const getDeviconClassName = (techName: string) => {
-  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();
-
-  return techMap[normalizedTechName]
-    ? `${techMap[normalizedTechName]} colored`
-    : "devicon-devicon-plain";
-};
-
-export const getTimeStamp = (createdAt: Date) => {
-  const date = new Date(createdAt);
-  const now = new Date();
-
-  const secondsAgo = Math.floor((now.getTime() - date.getTime()) / 1000);
-
-  const units = [
-    { label: "year", seconds: 31536000 },
-    { label: "month", seconds: 2592000 },
-    { label: "week", seconds: 604800 },
-    { label: "day", seconds: 86400 },
-    { label: "hour", seconds: 3600 },
-    { label: "minute", seconds: 60 },
-    { label: "second", seconds: 1 },
-  ];
-
-  for (const unit of units) {
-    const interval = Math.floor(secondsAgo / unit.seconds);
-    if (interval >= 1) {
-      return `${interval} ${unit.label}${interval > 1 ? "s" : ""} ago`;
-    }
-  }
-  return "just now";
-};
-
 export const techDescriptionMap: { [key: string]: string } = {
   javascript:
     "JavaScript is a powerful language for building dynamic, interactive, and modern web applications.",
@@ -73,4 +40,47 @@ export const getTechDescription = (techName: string) => {
   return techDescriptionMap[normalizedTechName]
     ? techDescriptionMap[normalizedTechName]
     : `${techName} is a technology or tool widely used in web development, providing valuable features and capabilities.`;
+};
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();
+
+  return techMap[normalizedTechName]
+    ? `${techMap[normalizedTechName]} colored`
+    : "devicon-devicon-plain";
+};
+
+export const getTimeStamp = (createdAt: Date) => {
+  const date = new Date(createdAt);
+  const now = new Date();
+
+  const secondsAgo = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  const units = [
+    { label: "year", seconds: 31536000 },
+    { label: "month", seconds: 2592000 },
+    { label: "week", seconds: 604800 },
+    { label: "day", seconds: 86400 },
+    { label: "hour", seconds: 3600 },
+    { label: "minute", seconds: 60 },
+    { label: "second", seconds: 1 },
+  ];
+
+  for (const unit of units) {
+    const interval = Math.floor(secondsAgo / unit.seconds);
+    if (interval >= 1) {
+      return `${interval} ${unit.label}${interval > 1 ? "s" : ""} ago`;
+    }
+  }
+  return "just now";
+};
+
+export const formatNumber = (number: number) => {
+  if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + "M";
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
 };
