f850b67cb0d5cb988029eeeea2739e49c43d5eae
Lenur Azizov <lenur29azizov@gmail.com>
2025-02-05 17:12:08 +0200

    feat: Implemented asnwer action

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -10,6 +10,7 @@ import { redirect } from "next/navigation";
 import React from "react";
 import { incrementViews } from "@/lib/actions/question.action";
 import { after } from "next/server";
+import AnswerForm from "@/components/forms/AnswerForm";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
@@ -87,6 +88,10 @@ const QuestionDetails = async ({ params }: RouteParams) => {
           />
         ))}
       </div>
+
+      <section className="my-5">
+        <AnswerForm questionId={question._id} />
+      </section>
     </>
   );
 };
